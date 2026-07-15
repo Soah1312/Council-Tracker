66b9a5cdcbe322f38e065e9d7c3ad97e905da6e3
Soah Fernandes <soah.fernandes2005@gmail.com>
2026-07-15 20:50:39 +0530

    oppsie

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,15 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import CouncilPortal from './pages/CouncilPortal';
 import AdminPanel from './pages/AdminPanel';
-import { ReactLenis } from 'lenis/react';
-
 function App() {
   return (
-    <ReactLenis root>
-      <Router>
+    <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/portal" element={<CouncilPortal />} />
         <Route path="/admin" element={<AdminPanel />} />
       </Routes>
     </Router>
-    </ReactLenis>
   );
 }
 
